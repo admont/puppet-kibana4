0589f6f8965a74874fdf2f581d1736f0193176de
Ilia Mashkov <Ilia.Mashkov@softronic.se>
2016-10-17 14:09:43 +0200

    Upgrade default version of Kibana repo

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,7 @@ class kibana4::params {
   $babel_cache_path              = '/tmp/babel.cache'
   $version                       = 'latest'
   $manage_repo                   = true
-  $package_repo_version          = '4.5'
+  $package_repo_version          = '4.6'
   $package_install_dir           = '/opt/kibana'
   $service_ensure                = true
   $service_enable                = true
